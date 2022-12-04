5ae08b8008d4cadc6c319a8d5baa4e2bb265fe68
Varrel Marcellius <varrel.marcellius@springkraf.com>
2022-12-05 01:32:01 +0700

    feat: add validation to category and task model

--- models/category.go
+++ models/category.go
@@ -8,7 +8,7 @@ import (
 
 type Category struct {
 	ID        uint      `json:"id" gorm:"primaryKey;type:integer"`
-	Type      string    `json:"type" gorm:"type:varchar(255)"`
+	Type      string    `json:"type" gorm:"type:varchar(255);not null"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
--- models/task.go
+++ models/task.go
@@ -10,8 +10,8 @@ import (
 
 type Task struct {
 	ID          uint   `json:"id" gorm:"primaryKey;type:integer"`
-	Title       string `json:"title" gorm:"type:varchar(255)"`
-	Description string `json:"description" gorm:"type:varchar(255)"`
+	Title       string `json:"title" gorm:"type:varchar(255);not null"`
+	Description string `json:"description" gorm:"type:varchar(255);not null"`
 	Status      bool   `json:"status" gorm:"type:boolean"`
 	UserID      uint   `json:"user_id"`
 	CategoryID  uint   `json:"category_id"`

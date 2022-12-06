36f5b3228afde859a0d19114ff51afba657ae14e
Varrel Marcellius <varrel.marcellius@springkraf.com>
2022-12-06 22:25:36 +0700

    rollback: trigger admin seeder

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"github.com/gin-gonic/gin"
 	"kanbanboard/controllers"
 	"kanbanboard/database"
@@ -18,17 +19,17 @@ func init() {
 }
 
 func handleArgs() {
-	//flag.Parse()
-	//args := flag.Args()
+	flag.Parse()
+	args := flag.Args()
 
-	//if len(args) >= 1 {
-	//	switch args[0] {
-	//	case "seeder":
-	userRepository := repositories.NewUserRepository(database.GetDB())
-	userSeed := seed.NewUserSeeder(userRepository)
-	userSeed.Execute()
-	//}
-	//}
+	if len(args) >= 1 {
+		switch args[0] {
+		case "seeder":
+			userRepository := repositories.NewUserRepository(database.GetDB())
+			userSeed := seed.NewUserSeeder(userRepository)
+			userSeed.Execute()
+		}
+	}
 }
 
 func main() {
